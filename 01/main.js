f8cd32f6920036561ff7ac1acded40ade888df3a
slastonm <slastonm@gmail.com>
2024-05-14 22:02:08 +0300

    01

---
+++ 01/main.js
@@ -0,0 +1,68 @@
+// let userName = 'John3333';
+// alert(userName);
+
+// let test = 'coment';
+var userEmail ='test@gmail.com';
+let fructName = 'orange';
+const KEYVALUE = 'secret key';
+
+let keyvalue = 123;
+let keyValueNumber = 321; //camelCase
+let KeyValue = 456;
+let key_value = 654;
+// let key-value = 90;
+
+keyvalue = 'my value';
+console.log('Hello console');
+console.log(key_value);
+console.log(KeyValue);
+console.log(keyvalue);
+
+// String
+
+let myStringValue = 'My name Tom';
+let myStringValueSecond = "my variable";
+console.log('Test string');
+console.log(myStringValue);
+console.log(myStringValueSecond);
+
+//Number 
+
+let myAge = 29;
+let saleValue = 0.5;
+let jarValue = 1.5;
+//NaN Not a number
+let nanValue = keyvalue/2;
+console.log(nanValue);
+let infinityValue = Infinity;
+let infinityResult = 1/0;
+console.log(infinityResult);
+
+// Boolean
+let isAdmin = true; // 2>1 ->true
+let isLogedUser = false; // 1>2
+
+//Null 
+let noValue = null;
+
+let notFindValue = undefined;
+
+//Math operation
+// - віднімання 
+// + додавання
+// / ділення
+// * множення
+// % залишок від ділення
+// ** піднесення до степеня
+
+let firstValue = 2;
+let secondValue = 3;
+
+let templateLiteral = `Hello js ${firstValue+secondValue}`;
+console.log(templateLiteral);
+console.log(`First value ${firstValue} + Second value: ${secondValue} = ${firstValue+secondValue}`);
+
+console.log(`- operation firstValue - seondValue = ${firstValue-secondValue}`);
+console.log(`* operation firstValue * seondValue = ${firstValue*secondValue}`);
+console.log(`/ operation firstValue / seondValue = ${firstValue/secondValue}`);
+
